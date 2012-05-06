de87e840ecfa18c898af30835397a29d11b5dede
Daniel Tull <dt@danieltull.co.uk>
2012-05-06 15:03:30 +0100

    Initial Commit

---
+++ DCTMenuController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DCTMenuController
+//
+//  Created by Daniel Tull on 06.05.2012.
+//  Copyright (c) 2012 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DCTMenuController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DCTMenuController
+//
+//  Created by Daniel Tull on 06.05.2012.
+//  Copyright (c) 2012 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
